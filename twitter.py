4a7d0bd9f04681dcd8951d0d73156feed274d922
David Excel Ozowara <de-ozowara@wiu.edu>
2023-06-25 23:34:42 +0000

    fix it.

--- twitter.py
+++ twitter.py
@@ -1,50 +1,37 @@
-import requests
 import os
-from pytwitter import Api
-import base64
+import tweepy
+from dotenv import load_dotenv
 
-BEARER_TOKEN = os.environ.get('TWITTER_BEARER_TOKEN')
-API_KEY = os.environ.get('TWITTER_API_KEY')
-API_KEY_SECRET = os.environ.get('TWITTER_API_KEY_SECRET')
-ACCESS_TOKEN = os.environ.get('TWITTER_TOKEN')
-ACCESS_SECRET = os.environ.get('TWITTER_TOKEN_SECRET')
+consumer_key = os.environ["TWITTER_API_KEY"]
+consumer_secret = os.environ["TWITTER_API_KEY_SECRET"]
+access_token = os.environ["TWITTER_TOKEN"]
+access_token_secret = os.environ["TWITTER_TOKEN_SECRET"]
 
-CLIENT_ID = os.environ.get('CLIENT_ID')
-CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
+auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
+auth.set_access_token(access_token,access_token_secret)
+ 
+api = tweepy.API(auth)
+ 
+try:
+    api.verify_credentials()
+    print('Successful Authentication')
+except:
+    print('Failed authentication')
 
-#api = Api(bearer_token=BEARER_TOKEN)
+me = api.verify_credentials() # Store user as a variable
+ 
+data = me.json()
 
-# api = Api(
-#   consumer_key=API_KEY,
-#   consumer_secret=API_KEY_SECRET,
-#   access_token=ACCESS_TOKEN,
-#   access_secret=ACCESS_SECRET
-# )
-
-base_url = 'https://api.twitter.com/'
-auth_url = 'https://api.twitter.com/oauth2/token'
-
-
-
+print(data)
 
 
-
-
-
-
-#response = requests.get(AUTH_URL, data=payload, )
-# print(response.json())
+# Get user Twitter statistics
+print(f"my.followers_count: {me.followers_count}")
+print(f"my.listed_count: {me.listed_count}")
+print(f"my.statuses_count: {me.statuses_count}")
 
 
 
 
-# if response.status_code == 200:
-#     # Request was successful
-#     print('POST request successful!')
-#     print(response.json())
-# else:
-#     # Request failed
-#     print('POST request failed. Status code:', response.status_code)
+print(me.followers_count)
 
-
-#https://developer.twitter.com/en/portal/projects/1672815275652022272/apps/27370138/settings
